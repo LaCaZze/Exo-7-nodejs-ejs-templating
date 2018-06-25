45f2b10b1fb4729c61a47be030de1b1306c1163d
Michael <michaelissaly@gmail.com>
2018-06-25 14:55:59 +0400

    Liaison au fichier Json et bouclage dessus

--- serveur.js
+++ serveur.js
@@ -40,6 +40,9 @@ console.log('3254 is the magic port'); */
 // load the things we need
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+var fs = require('fs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -49,28 +52,54 @@ app.set('view engine', 'ejs');
 // index page 
 // app.get('/', function(req, res) {
 //     res.render('pages/index');
-    
+
 // });
 
 // index page 
-app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'html', level: 3 },
-        { name: 'CSS', level: 5 },
-        { name: 'JS', level: 10 }
-    ];
+// app.get('/', function(req, res) {
+//     var drinks = [
+//         { name: 'html', level: 3 },
+//         { name: 'CSS', level: 5 },
+//         { name: 'JS', level: 10 }
+//     ];
 
 
-    res.render('pages/index', {
-        drinks: drinks,
-    });
+//     res.render('pages/index', {
+//         drinks: drinks,
+//     });
+// });
+
+app.get('/about', function (req, res) {
+   
+
+    var competences= JSON.parse(fs.readFileSync('competences.json', 'utf8'));
+
+    res.render('pages/about',{
+        competences: competences,
+    })
 });
+
+// app.get('/', function (req, res) {
+
+//     let request = require('request');
+//     let url = `http://localhost:8280/list`
+
+//     request(url, function (err, response, body) {
+//         if (err) {
+//             console.log('error:', error);
+//         } else {
+//             console.log('body:', body);
+//         }
+//         res.render(JSON.parse(body))
+//     });
+
+// });
 
 
 // about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
+// app.get('/about', function(req, res) {
+//     res.render('pages/about');
+// });
 
 app.listen(8280);
 console.log('8280 is the magic port');
